1be6010827dfbec0eadec1f13a00f5fc85fcbe42
Vivek Raj <rajviv79@gmail.com>
2025-01-04 11:13:10 -0600

    updated_Techstool

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
 import { DiJavascript1, DiReact, DiNodejs, DiPython, DiGit, DiJava } from "react-icons/di";
-import { SiTensorflow, SiPytorch, SiScikitlearn, SiLinux, SiSap, SiC } from "react-icons/si";
+import { SiTensorflow, SiPytorch, SiScikitlearn, SiLinux, SiSap, SiC, SiMongodb, SiFlask, SiAdobe } from "react-icons/si";
 import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
@@ -43,6 +43,15 @@ function Techstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFlask />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAdobe title="UI/UX" />
       </Col>
     </Row>
   );
