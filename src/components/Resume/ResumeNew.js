7aca65a098ac37a0e1a082ea435ac55c84517dfd
Vivek Raj <rajviv79@gmail.com>
2025-01-17 11:57:40 -0500

    fixed nav bar

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Vivek_Raj_HC_RESUME_V1.pdf";
+import pdf from "../../Assets/../Assets/Master_M_Vivek_Raj_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
